c0a8018dbe63d46ce88fe3b7678a94c502ea7f2f
Jwaugh3 <jwaugh3@gmail.com>
2020-11-29 10:06:27 -0600

    added ModLog Component and styles

--- client/src/Components/ModuleContainer/ModLog/ModerationLog/ModRecord/ModRecord.js
+++ client/src/Components/ModuleContainer/ModLog/ModerationLog/ModRecord/ModRecord.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 //Styling
 import styles from './ModRecord.module.css';
+//Assets
+import modIcon from '../../../../../resources/lightModIcon.png';
 
 class ModRecord extends Component {
     render() {
         return (
-            <div>
-                <h1>test</h1>
+            <div className={styles.recordContainer}>
+                <div className={styles.headerContainer}>
+                    <p className={styles.modUsername}>{this.props.modUsername}</p>
+                    <img src={modIcon} alt="mod Icon" className={styles.modIcon}/>
+                </div>
+                <div className={styles.recordSpacer}></div>
+                <div className={styles.bodyContainer}>
+                    <div className={styles.recordTime}>
+                        {this.props.time}
+                    </div>
+                    <div className={styles.recordAction}>
+                        {this.props.action}
+                    </div>
+                </div>
             </div>
         )
     }
--- client/src/Components/ModuleContainer/ModLog/ModerationLog/ModerationLog.js
+++ client/src/Components/ModuleContainer/ModLog/ModerationLog/ModerationLog.js
@@ -10,10 +10,14 @@ export class ModerationLog extends Component {
         return (
             <div className={styles.mainContainer}>
                 <div className={styles.headerContainer}>
-                    <h3 className={styles.chatHeader}>Moderation Log</h3>
+                    <h3 className={styles.logHeader}>Moderation Log</h3>
                 </div>
                 <div className={styles.logContainer}>
-                    <ModRecord/>
+                    <ModRecord modUsername={'Jwaugh3'} time={'Nov 12, 8:15pm'} action={'this is filler text, you shouldnt read any of this text if you want to be productive'}/>
+                    <ModRecord modUsername={'Jwaugh3adsfasdfasd'} time={'Jul 23, 12:15pm'} action={'this is filler text, you shouldnt read any of this text if you want to be productive'}/>
+                    <ModRecord modUsername={'Jwaugh3asdf'} time={'Nov 12, 8:15pm'} action={'this is filler text, you shouldnt read any of this text if you want to be productiveasdfasdf sadf asdfasdfas asdfda asdffasdf asdfafasd asdfasdfa asdfasdfsa asdfsafdas sadfasdfdasf asdf'}/>
+                    <ModRecord modUsername={'Jwaugh3dfa'} time={'Nov 12, 8:15pm'} action={'this is filler text, you shouldnt read any of this text if you want to be productive'}/>
+                    <ModRecord modUsername={'Jwau'} time={'Nov 12, 8:15pm'} action={'this is filler text, you shouldnt'}/>
                 </div>
             </div>
         )
